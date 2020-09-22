88e331e25bbe4c057cd3970ab471a95e48e252a1
ivar <ivar@mail.chungyo.net>
2020-09-22 21:37:10 +0800

    [修改]補充self static範例

--- learn/self_and_static.php
+++ learn/self_and_static.php
@@ -37,3 +37,35 @@ Man::callself();  // xiaosan
 //slef
 //子类继承父类方法，其实并不是把父类方法拿过来，而是有了父类方法的访问权限，
 //当调用子类里没有的方法时，会去父类里找，找到后在父类里执行，自然就self指向父类
+
+
+echo '\n';
+class A {
+    private function foo() {
+        echo "success!\n";
+    }
+    public function test() {
+        self::foo();
+        $this->foo();
+        static::foo();
+    }
+}
+
+class B extends A {
+   /* foo() will be copied to B, hence its scope will still be A and
+    * the call be successful */
+}
+
+class C extends A {
+    public function foo() {
+        echo '123\n';
+        /* original method is replaced; the scope of the new one is C */
+    }
+}
+
+$b = new B();
+$b->test();
+$c = new C();
+$c->test();   //fails
+
+
