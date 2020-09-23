fd10933c19478093d81ca51a89d63c8bbf2419a7
ivar <ivar@mail.chungyo.net>
2020-09-23 14:32:51 +0800

    [建立] inspire單元測試，以及依賴注入方式進行實作

--- learn/self_and_static.php
+++ learn/self_and_static.php
@@ -7,12 +7,17 @@ class Person
     }
     public static function callself()
     {
-        self::name();
+        return self::name();
     }
 
     public static function callstatic()
     {
-        static::name();
+        return static::name();
+    }
+
+    public function callthis()
+    {
+        return $this->name();
     }
 }
 
@@ -33,11 +38,19 @@ Man::callstatic();  // gaojin
 echo '<br>';
 Man::callself();  // xiaosan
 
-
-//slef
-//子类继承父类方法，其实并不是把父类方法拿过来，而是有了父类方法的访问权限，
-//当调用子类里没有的方法时，会去父类里找，找到后在父类里执行，自然就self指向父类
-
+$newname = new Man();
+echo "====\n";
+echo $newname->callthis() . "\n";
+/**
+ * slef
+ * 子类继承父类方法，其实并不是把父类方法拿过来，而是有了父类方法的访问权限，
+ * 当调用子类里没有的方法时，会去父类里找，找到后在父类里执行，自然就self指向父类
+ * (1).self是參照到目前的class，$this是參照到目前的object ( 已經被宣告的實體上 )
+ * (2).self 可使用在static上，$this不行
+ * static method 因為沒有物件的實體，所以需要注意不可以使用 $this ，要用self::
+ *  可以直接存取 static method ( 如self::method() )，但是無法直接存取 static property 中的預先宣告的值
+ * (3). 可用 new self() 呼叫自己
+ */
 
 echo '\n';
 class A {
