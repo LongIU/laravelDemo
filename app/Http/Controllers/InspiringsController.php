f721400d5eef15d9d94d53fa64450db60ddbdba1
ivar <ivar@mail.chungyo.net>
2020-09-24 17:44:36 +0800

    [安裝]install laravel
    composer require laravel/ui
    
    php artisan ui vue --auth
    
    npm install && npm run dev

---
+++ app/Http/Controllers/InspiringsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\InspiringService;
+use Illuminate\Http\Request;
+
+class InspiringController extends Controller
+{
+    private $service;
+    public function __construct(InspiringService $inspiringService)
+    {
+       $this->service = $inspiringService;
+    }
+
+    public function inspire()
+    {
+       return $this->service->inspire();
+
+    }
+}
